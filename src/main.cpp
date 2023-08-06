4c88b2e0fdb872952a074213e22c64c368026d4b
Will Tatam <will@netmindz.net>
2023-08-06 11:15:47 +0100

    ArtNet depends on APPMOD_LEDS

--- src/main.cpp
+++ src/main.cpp
@@ -25,6 +25,9 @@
 #ifdef APPMOD_LEDS
   #include "App/AppModLeds.h"
   #include "App/AppModLedFixGen.h"
+  #ifdef USERMOD_ARTNET
+    #include "User/UserModArtNet.h"
+  #endif
 #endif
 #ifdef USERMOD_E131
   #include "User/UserModE131.h"
@@ -48,6 +51,9 @@ void setup() {
   #ifdef APPMOD_LEDS
     lds = new AppModLeds();
     lfg = new AppModLedFixGen();
+    #ifdef USERMOD_ARTNET
+      artnetmod = new UserModArtNet();
+    #endif
   #endif
   #ifdef USERMOD_E131
     e131mod = new UserModE131();
@@ -66,6 +72,9 @@ void setup() {
   mdls->add(print);
   #ifdef APPMOD_LEDS
     mdls->add(lfg);
+    #ifdef USERMOD_ARTNET
+      mdls->add(artnetmod);
+    #endif
   #endif
   mdls->add(ui);
   mdls->add(web);

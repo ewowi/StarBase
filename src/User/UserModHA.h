6549f3e132982ee8ff34ee74c50b9a92981dcd82
Will Tatam <will@netmindz.net>
2024-04-23 21:45:10 +0100

    Add sample sensor

--- src/User/UserModHA.h
+++ src/User/UserModHA.h
@@ -17,16 +17,18 @@ class UserModHA:public SysModule {
 
 public:
 
-  UserModHA() :SysModule("Home Assistant support - template") {
+  UserModHA() :SysModule("Home Assistant support") {
     isEnabled = false;
   };
 
-  void setup() {
+  void setup() override {
     SysModule::setup();
 
     parentVar = ui->initUserMod(parentVar, name, 6300);
 
     ui->initText(parentVar, "mqttAddr");
+    ui->initText(parentVar, "mqttUser");
+    ui->initText(parentVar, "mqttPass");
   }
 
 
@@ -34,19 +36,31 @@ public:
     ppf("connectedChanged\n");
     if (mdls->isConnected) {
       // set device's details (optional)
-      device.setName(_INIT(TOSTRING(APP)));
+      device.setName(mdl->getValue("instance"));
       device.setSoftwareVersion(_INIT(TOSTRING(VERSION)));
-    }
+   }
 
-    byte mac[] = {0xF1, 0x10, 0xFA, 0x6E, 0x38, 0x4A}; // TODO
+    byte mac[6];
+    WiFi.macAddress(mac);
     device.setUniqueId(mac, sizeof(mac));
 
     String mqttAddr = mdl->getValue("mqttAddr");
+    String mqttUser = mdl->getValue("mqttUser");
+    if(mqttUser == "null" || mqttUser == nullptr) mqttUser = "";
+    String mqttPass = mdl->getValue("mqttPass");
+    if(mqttPass == "null" || mqttPass == nullptr) mqttPass = "";
 
-    ppf("mqtt->begin(%s)\n", mqttAddr.c_str());
     IPAddress ip;
     if(ip.fromString(mqttAddr)) {
-      mqtt->begin(ip, "", "");
+      if(mqttUser == "") {
+        ppf("mqtt->begin('%s')\n", mqttAddr.c_str());
+        mqtt->begin(ip);
+      }
+      else {
+        ppf("WARNING - untested mqtt->begin('%s', '%s', pass)\n", mqttAddr.c_str(), mqttUser.c_str());
+        mqtt->begin(ip, mqttUser.c_str(), mqttPass.c_str());  
+      }
+      started = true;
     }
     else {
       ppf("Failed to parse %s to IP\n", mqttAddr.c_str());
@@ -54,15 +68,22 @@ public:
 
   }
 
-  void loop() {
+  void loop() override {
     // SysModule::loop();
     mqtt->loop();
+  }
+
+  void loop10s() override {
+    if(!started) return;
+    testSensor->setValue((uint32_t) (millis() / 1000));
   }
 
   private:
     WiFiClient client;
     HADevice device;
     HAMqtt* mqtt = new HAMqtt(client, device);
+    HASensorNumber* testSensor = new HASensorNumber("uptime");
+    bool started = false;
 };
 
 extern UserModHA *hamod;
